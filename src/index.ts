b686294365d37adc5e899d20a41e87c58bb8792f
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-22 13:46:39 -0300

    Criando meu TODO e Reminder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-alert('Teste');
+( () => {
+    const todo ={
+        // Descrição do objeto
+        // DONE = é a fleg que marca se ja foi feito ou não
+
+        description: 'todo',
+        done: false
+    }
+
+    const reminder = {
+        description: 'reminder',
+        date: '22.11.2024'
+    }
+})()
