5598e6b6a476d2f946ab9f5d870b676b1751c4c0
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-11-27 14:38:26 -0300

    update

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,28 +1,53 @@
 "use strict";
 (function () {
-    var todo = {
-        // Descrição do objeto
-        // DONE = é a fleg que marca se ja foi feito ou não
-        description: 'todo',
-        done: false
-    };
+    var Reminder = /** @class */ (function () {
+        function Reminder(description, date, notifications) {
+            this.id = '';
+            this.dateCreated = new Date();
+            this.dateUpdated = new Date();
+            this.description = '';
+            this.date = new Date();
+            this.notifications = ['EMAIL'];
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+        Reminder.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Reminder;
+    }());
+    var Todo = /** @class */ (function () {
+        function Todo(description) {
+            this.id = '';
+            this.dateCreated = new Date();
+            this.dateUpdated = new Date();
+            this.description = '';
+            this.done = false;
+            this.description = description;
+        }
+        Todo.prototype.render = function () {
+            return JSON.stringify(this);
+        };
+        return Todo;
+    }());
+    // Descrição do objeto
+    // DONE = é a fleg que marca se ja foi feito ou não
+    var todo = new Todo('Tudo criado com classe');
     // Para lembrete
-    var reminder = {
-        description: 'reminder',
-        date: '22.11.2024'
-    };
+    var reminder = new Reminder('Reminder criado com classe', new Date(), ['EMAIL']);
     // Onde o código se comunica com a interface
     var taskView = {
         // Reder vai receber uma lista, que será os (todos e remindes)
-        reder: function (tasks) {
-            var taskList = document.querySelector('#tasksList');
+        render: function (tasks) {
+            var taskList = document.getElementById('tasksList');
             // para limpar os elementos usaremos while
             while (taskList === null || taskList === void 0 ? void 0 : taskList.firstChild) {
                 taskList.removeChild(taskList.firstChild);
             }
-            tasks.forEach(function (tasks) {
+            tasks.forEach(function (task) {
                 var li = document.createElement('li');
-                var textNode = document.createTextNode(JSON.stringify(tasks));
+                var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 taskList === null || taskList === void 0 ? void 0 : taskList.appendChild(li);
             });
@@ -34,7 +59,7 @@
         var tasks = [todo, reminder];
         var handleEvent = function (event) {
             event.preventDefault();
-            view.reder(tasks);
+            view.render(tasks);
         };
         (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
     };
