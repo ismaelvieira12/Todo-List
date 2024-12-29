7af21af7d6d3913378791f555eef0836d9d03521
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-29 12:12:39 -0300

    reorganizando as cores

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import TodoTask from './components/Todo/TodoTask'
+import "../src/style/style.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <div className="App">
+
+      <header>
+
+        <h2 className='title'>Gerenciador de tarefas</h2>
+
+        <input
+          type="text" autoComplete="off" 
+          placeholder="Adicione uma tarefa" 
+          name="task"
+          className="input"
+        />
+
+        <button type="submit" className="btn-header">Adicionar tarefa</button>
+      </header>
+
+      <div className="line"></div>
+
+      <TodoTask />
+
+    </div>
+    
   )
 }
 
