35f09e42ecab35f47f5784efab252b3f6767d3a6
Ismael Nasciemnto <ismaelvieira12@outlook.com>
2024-12-29 11:21:46 -0300

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import TodoTask from "./components/TodoTask/TodoTask";
-
-import './styles/styles.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
-	return (
-		<div className="App">
-
-			<header>
-
-				<h2>Lists</h2>
-
-				<input
-					type="text" autoComplete="off" 
-					placeholder="Escrever task..." 
-					name="task"
-					className="input"
-				/>
-
-				<button type="submit" className="btn-header">Adicionar Task</button>
-			</header>
-			
-			<div className="line"></div>
-
-			<TodoTask />
-			
-		</div>
-	);
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
